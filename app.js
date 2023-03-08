8517b0bfa4389b8c34928f7f59f818830bf99631
Oleksandr P <goodwin009@gmail.com>
2023-03-08 18:19:41 +0200

    add CRUD Mongo
    add patch for favorite
    add CRUD Mongo

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+<<<<<<< Updated upstream
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require('./routes/api/contacts')
+=======
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+require("colors");
+
+const contactsRouter = require("./src/routes/contacts.routes");
+>>>>>>> Stashed changes
 
 const app = express()
 
@@ -12,7 +22,13 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+<<<<<<< Updated upstream
 app.use('/api/contacts', contactsRouter)
+=======
+app.use(require("./src/middlewares/errorHandler"));
+app.use("/api/contacts", contactsRouter);
+app.use(require("./src/middlewares/badUrlError"));
+>>>>>>> Stashed changes
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+<<<<<<< Updated upstream
 const app = require('./app')
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+=======
+const app = require("./app");
+const { connectDB } = require("./src/config/connection");
+
+const PORT = process.env.PORT || 8081;
+
+(async () => {
+  await connectDB();
+})();
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`.bgBlue);
+});
+>>>>>>> Stashed changes
